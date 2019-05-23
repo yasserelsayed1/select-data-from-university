0072bdabca99ebdffd4c42a9d58ff154fa2b21c3
Abdelrahman Sobhy Saleh <44747476+sobhyabdo@users.noreply.github.com>
2019-05-24 01:33:31 +0200

    Add files via upload

---
+++ University system project/src/university/project/ServerUniversity.java
@@ -0,0 +1,33 @@
+
+package university.project;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class ServerUniversity {
+    
+    public static void main(String[] args){    
+        try {
+            
+            ServerSocket server = new ServerSocket(1123);
+
+            while(true){
+                try {
+                    Socket socket = server.accept();
+                    ServerThreadUni thread = new ServerThreadUni(socket);
+                    thread.start();
+                  } catch (IOException ex) {     
+                    System.out.println("Error" + ex.getMessage());
+                  }
+            }
+            
+        } catch (IOException ex) {
+            System.out.println("Fetal error : " + ex.getMessage());
+        }
+    }
+    
+}
